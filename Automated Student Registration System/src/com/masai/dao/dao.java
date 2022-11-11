b57a57af06ee8d20997f7ca49ab5b48259731a2b
NagArindam <inspkolkata12@gmail.com>
2022-11-11 23:36:46 +0530

    Day4

--- Automated Student Registration System/src/com/masai/dao/dao.java
+++ Automated Student Registration System/src/com/masai/dao/dao.java
@@ -1,10 +1,27 @@
 package com.masai.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.masai.beanClass.Courses;
 import com.masai.beanClass.Students;
+import com.masai.exceptions.CoursesException;
 import com.masai.exceptions.StudentsException;
 
 public interface dao {
 
 	
 	public String registerStudent (Students student) throws StudentsException;
+	public boolean registerBatch (int cid, String cname, String semail) throws CoursesException;
+	public boolean checkStudent (String semail, String spassword) throws StudentsException;
+	public List<Courses> ListOfCourse () throws CoursesException;
+	public int getCourseId (String cname) throws CoursesException;
+	public boolean checkCourse (String cname) throws CoursesException;
+	public boolean EditStudentDetail (String semail, String spassword, String snewpassword, String snewname, String snewcourse) throws CoursesException;
+	
+	public void EditStudentProfile (String semail, String stdnewpassword, String stdnewname) throws StudentsException;
+	public List<batchSeatDTO> getBatchSeatDetails() throws SQLException;
+	public String getCoursename (int cid) throws CoursesException;
+	public void displayCourseAvilabalityWithOrWithoutSeats (String option) throws SQLException;
+	
 }
--- Automated Student Registration System/src/com/masai/dao/daoImpl.java
+++ Automated Student Registration System/src/com/masai/dao/daoImpl.java
@@ -3,8 +3,11 @@ package com.masai.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
+import com.masai.beanClass.Courses;
 import com.masai.beanClass.Students;
+import com.masai.exceptions.CoursesException;
 import com.masai.exceptions.StudentsException;
 import com.masai.utility.DButil;
 
@@ -34,6 +37,77 @@ public class daoImpl implements dao{
 		}
 		
 		return result;
+	}
+
+	@Override
+	public boolean registerBatch(int cid, String cname, String semail) throws CoursesException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkStudent(String semail, String spassword) throws StudentsException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Courses> ListOfCourse() throws CoursesException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCourseId(String cname) throws CoursesException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean checkCourse(String cname) throws CoursesException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean EditStudentDetail(String semail, String spassword, String snewpassword, String snewname,
+			String snewcourse) throws CoursesException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void EditStudentProfile(String semail, String stdnewpassword, String stdnewname) throws StudentsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getCoursename(int cid) throws CoursesException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void displayCourseAvilabalityWithOrWithoutSeats(String option) throws SQLException{
+		
+		dao daoObjecDao = new daoImpl();
+		List<batchSeatDTO> listOfBatchSeats;
+		
+		listOfBatchSeats = daoObjecDao.getBatchSeatDetails();
+		int n = listOfBatchSeats.size();
+		int[] cidIndex = new int[n];
+		int[] filledSeats = new int[n];
+		int[] totalSeats = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			cidIndex[i] = listOfBatchSeats.get(i).getcid();
+			filledSeats[i] = listOfBatchSeats.get(i).getfilledSeats();
+			totalSeats[i] = listOfBatchSeats.get(i).getTotalSeats();
+		}
+		
+		
+		
 	}
 
 }
